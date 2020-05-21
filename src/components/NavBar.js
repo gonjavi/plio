031edd33621ab0fa67f814c1970bc0babae57abf
gonjavi <javiercaliescali@gmail.com>
2020-05-21 16:36:25 -0500

    work on the navbar and background

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="nav-wrapper white">
+        <h4 className="green-text">G. Javier</h4>
+      </nav>
+    )
+  }
+}
+
+export default NavBar;
