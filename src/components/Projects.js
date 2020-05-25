57b52a5d38e6f0954d5be3bb97f5d8420d48ee1a
gonjavi <javiercaliescali@gmail.com>
2020-05-25 15:17:20 -0500

    change photos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,11 @@ import todolist from '../img/todolist.png';
 import facebook from '../img/facebook.png';
 import resto from '../img/resto.png';
 import weather from '../img/weather.png';
+import medium1 from '../img/medium1.png';
+import newsweek from '../img/newsweek.png';
+import newyork from '../img/newyork.png';
+import apple from '../img/apple.png';
+
 
 class Projects extends Component {
 
@@ -17,33 +22,61 @@ class Projects extends Component {
 
         <div className="row">
         
-          <div className="col-6 ">
+          <div className="col-md-6 col-sm-12">
           <img
             alt='todolist'
             width = '100%'
             src={todolist}
           />
           </div>
-          <div className="col-6">
+          <div className="col-md-6 col-sm-12 ">
           <img
             alt='todolist'
             width = '100%'
             src={resto}
             />
           </div>
-          <div className="col-6">
+          <div className="col-md-6 col-sm-12 ">
           <img
             alt='todolist'
             width = '100%'
             src={facebook}
             />
           </div>
-          <div className="col-6">
-          <img
-            alt='todolist'
-            width = '100%'
-            src={weather}
-            />
+          <div className="col-md-6 col-sm-12 ">
+            <img
+              alt='todolist'
+              width = '100%'
+              src={weather}
+              />
+          </div>
+          <div className="col-md-6 col-sm-12 ">
+            <img
+              alt='medium'
+              width = '100%'
+              src={medium1}
+              />
+          </div>
+          <div className="col-md-6 col-sm-12 mb-15">
+            <img
+              alt='apple'
+              width = '100%'
+              src={apple}
+              />
+          </div>
+          <div className="col-md-6 col-sm-12">
+            <img
+              alt='newyork'
+              width = '100%'
+              src={newyork}
+              />
+          </div>
+          <div className="col-md-6 col-sm-12 ">
+            <img
+              alt='newsweek'
+              width = '100%'
+              src={newsweek}
+              />
           </div>
 
         </div>
