12f621177e1c147eacdbdfcfbe9f53b3a55e225a
gonjavi <javiercaliescali@gmail.com>
2020-05-26 11:32:22 -0500

    add links to photos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,6 +7,7 @@ import medium1 from '../img/medium1.png';
 import newsweek from '../img/newsweek.png';
 import newyork from '../img/newyork.png';
 import apple from '../img/apple.png';
+import { Link } from 'react-router-dom';
 
 
 class Projects extends Component {
@@ -24,66 +25,82 @@ class Projects extends Component {
         
           <div className="col-md-6 col-sm-12 columnas">
             <div>
-              <img
-                alt='todolist'
-                width = '100%'
-                src={todolist}
-              />
+              <a target="_blank" rel="noopener noreferrer" href="https://gonjavi.github.io/JavascriptTodoList/">
+                <img
+                  alt='todolist'
+                  width = '100%'
+                  src={todolist}
+                />
+              </a>
             </div>
 
             <div>
-              <img
-                alt='todolist'
-                width = '100%'
-                src={facebook}
-                />
+              <a target="_blank" rel="noopener noreferrer" href="https://bookfaces1.herokuapp.com/">
+                <img
+                  alt='facebookapp'
+                  width = '100%'
+                  src={facebook}
+                  />
+                </a>
             </div>
             <div className="">
-              <img
-                alt='medium'
-                width = '100%'
-                src={medium1}
-                />
+              <a target="_blank" rel="noopener noreferrer" href="https://gonjavi.github.io/cssframework/">
+                <img
+                  alt='medium'
+                  width = '100%'
+                  src={medium1}
+                  />
+              </a>
             </div>
             <div className="">
-              <img
-                alt='newyork'
-                width = '100%'
-                src={newyork}
-                />
+              <a target="_blank" rel="noopener noreferrer" href="https://gonjavi.github.io/HTML5CSS3NewYorkTimes/">
+                <img
+                  alt='newyork'
+                  width = '100%'
+                  src={newyork}
+                  />
+              </a>
             </div>
 
           </div>
           <div className="col-md-6 col-sm-11 columnas">
             <div>
+            <a target="_blank" rel="noopener noreferrer" href="https://gonjavi.github.io/JavascriptRestaurantPage/">
               <img
-                alt='todolist'
+                alt='restaurant'
                 width = '100%'
                 src={resto}
                 />
+              </a>
             </div>
             <div>
-              <img
-                alt='todolist'
-                width = '100%'
-                src={weather}
-                />
+              <a target="_blank" rel="noopener noreferrer" href="https://gonjavi.github.io/JavaScriptWeatherApp/">
+                <img
+                  alt='weatherapp'
+                  width = '100%'
+                  src={weather}
+                  />
+              </a>
             </div>
          
             <div>
-              <img
-                alt='apple'
-                width = '100%'
-                src={apple}
-                />
+              <a target="_blank" rel="noopener noreferrer" href="https://gonjavi.github.io/HTML5CSS3ApplePage/">
+                <img
+                  alt='apple'
+                  width = '100%'
+                  src={apple}
+                  />
+              </a>
             </div>
             
             <div>
-              <img
-                alt='newsweek'
-                width = '100%'
-                src={newsweek}
-                />
+              <a target="_blank" rel="noopener noreferrer" href="https://gonjavi.github.io/HMTL5BOOTSTRAPNewsweek/">
+                <img
+                  alt='newsweek'
+                  width = '100%'
+                  src={newsweek}
+                  />
+              </a>
             </div>
           </div>
 
