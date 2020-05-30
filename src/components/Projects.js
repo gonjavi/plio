835f588f7bb617cf85cfbb38684f728c10cfd752
gonjavi <javiercaliescali@gmail.com>
2020-05-29 20:21:55 -0500

    changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,27 +21,32 @@ class Projects extends Component {
                 <div className="img img2">
                     <div class="background-gradient">
                       <div class="text-img">
+                      <a target="_blank" rel="noopener noreferrer" href="https://github.com/gonjavi/railsfacebookapp">
                         <p>Ruby on Rails</p>
                         <p>PRODUCT - FACEBOOKAPP</p>
                         <p>JavaScript</p>
+                      </a>
                       </div>
                     </div>
                   </div>
                 </a>
             </div>
             <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://gonjavi.github.io/HTML5CSS3ApplePage/">
-              <div className="img img7">
+              <a target="_blank" rel="noopener noreferrer" href="https://gonjavi.github.io/HMTL5BOOTSTRAPNewsweek/">
+              <div className="img img8">
                   <div class="background-gradient">
                     <div class="text-img">
+                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/gonjavi/HMTL5BOOTSTRAPNewsweek">
                       <p>HTML - CSS3</p>
-                      <p>PRODUCT - APPLE</p>
+                      <p>PRODUCT - NEWSWEEK</p>
                       <p></p>
+                      </a>
                     </div>
                   </div>
                 </div>
               </a>
             </div>
+
             
           </div>
           <div className="col-md-6 col-sm-11 columnas">
@@ -50,9 +55,11 @@ class Projects extends Component {
                <div className="img img5">
                   <div class="background-gradient">
                     <div class="text-img">
+                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/gonjavi/javascriptrestaurantpage">
                       <p>JavaScript</p>
                       <p>PRODUCT - RESTAURANT</p>
                       <p></p>
+                    </a>
                     </div>
                   </div>
                 </div>
@@ -63,9 +70,11 @@ class Projects extends Component {
                 <div className="img img6">
                   <div class="background-gradient">
                     <div class="text-img">
+                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/gonjavi/javascriptweatherapp"> 
                       <p>JavaScript</p>
                       <p>PRODUCT - WEATHERAPP</p>
                       <p></p>
+                    </a>
                     </div>
                   </div>
                 </div>
