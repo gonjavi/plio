92955f5467f0b7af85eb349aed9c1560ced5b789
gonjavi <javiercaliescali@gmail.com>
2020-05-30 19:57:27 -0500

    style about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import morgan from '../img/morgan.jpg';
+import us from '../img/us.jpg';
 
 class About extends Component {
   render() {
@@ -7,18 +9,27 @@ class About extends Component {
         
         <div className="row">
           <h1 className="col-12 text-center">About</h1>
-          <div className="col-10 offset-1 texto">
+
+          <div className="col-md-6 picture1">
+             <img 
+             className = 'pictureabout'
+              alt = 'contact'
+              src = {us} 
+             />
+          </div>
+
+          <div className="col-lg-6 col-md-12 col-sm-12  texto">
           Software Engineer/Full-stack Developer
           Main stack: Ruby on Rails, JavaScript, React and Redux.
           Open to new full-stack development opportunities in global Tech.
-          </div>
-        </div>
-        <div className="row">
-        <div className="col-10 offset-1 texto">
+          
+        
+       
+        <div >
           A multi-skilled web developer who prioritizes commitment, responsibility, teamwork, writing/speaking skills, and a passion for development.
 
           </div>
-          <div className="col-10 offset-1 texto">
+          <div >
           I've developed knowledge in:
           <ul>
             <li>Full-stack development (Ruby on Rails, JavaScript, React)</li>
@@ -29,11 +40,12 @@ class About extends Component {
             <li>MySQL</li>
             <li>Object-Oriented Programming</li>
           </ul>
-            Passionate about solving problems and learning new things. Open to exploring exciting full-stack development opportunities. I love talking with other devs; feel free to connect if you'd like to chat. If you'd like to get in contact about potential job opportunities, please reach out!
+            Passionate about solving problems and learning new things. Open to exploring exciting full-stack development opportunities. If you'd like to get in contact about potential job opportunities, please reach out!
           </div>
         </div>
+        </div>
         
-      </div>
+        </div>
     )
   }
 }
