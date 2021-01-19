95c168c4cedabba8afab1e8f213c2c1514bbf6fd
manci <javiercaliescali@gmail.com>
2021-01-19 09:19:58 -0500

    animar titulos

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,8 +5,8 @@ class Intro extends Component {
     return (
       <div className="container">
         <div className="row">
-          <p className="intro col-12 offset-lg-1">G. Javier</p>
-          <p className="intro col-lg-9 offset-lg-1 col-sm-12">I'm Gonzalo, <span> a software engineer and full-stack developer</span> living in Cali. I'm passionate about coding. </p>
+          <p className="intro gjavier col-12 offset-lg-1">G. Javier</p>
+          <p className="intro Imgonzalo col-lg-9 offset-lg-1 col-sm-12">I'm Gonzalo, <span> a software engineer and full-stack developer</span> living in Cali. I'm passionate about coding. </p>
         </div>
         
       </div>
