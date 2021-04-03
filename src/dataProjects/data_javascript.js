f817a7326df5f233697cc9f61e49b96751c3bab9
manci <javiercaliescali@gmail.com>
2021-04-02 20:26:03 -0500

    actualizar proyectos javascript

--- src/dataProjects/data_javascript.js
+++ src/dataProjects/data_javascript.js
@@ -2,13 +2,14 @@ import resto from '../img/javascript/resto.png';
 import library from '../img/javascript/library.png';
 import weather from '../img/javascript/weather.png';
 import todolist from '../img/javascript/todolist.png';
+import tictactoe2 from '../img/javascript/tictactoe2.png';
 
 const dataProjects = [
   {
     id: 0,
     technologies: 'JAVASCRIPT',
     titulo: 'RESTAURANT PAGE',
-    description: 'The entire content of the website is generated with JavaScript',
+    description: 'The entire content of the website is generated with JavaScript.',
     demoLink: 'https://gonjavi.github.io/JavascriptRestaurantPage/',
     githubLink: 'https://github.com/gonjavi/JavascriptRestaurantPage',
     img: resto,
@@ -17,7 +18,7 @@ const dataProjects = [
     id: 1,
     technologies: 'JavaScript - HTML - CSS3',
     titulo: 'LIBRARY APP',
-    description: 'An app based on javascript, applying concepts of OOP',
+    description: 'An app based on javascript, applying concepts of OOP.',
     demoLink: 'https://gonjavi.github.io/JavascriptLibrary/',
     githubLink: 'https://github.com/gonjavi/JavascriptLibrary',
     img: library,
@@ -26,7 +27,7 @@ const dataProjects = [
     id: 2,
     technologies: 'JavaScript - HTML - Boostrap',
     titulo: 'WEATHER APP',
-    description: 'A weather forecast site built using the Open Weather Map API',
+    description: 'A weather forecast site built using the Open Weather Map API.',
     demoLink: 'https://gonjavi.github.io/JavaScriptWeatherApp/',
     githubLink: 'https://github.com/gonjavi/JavaScriptWeatherApp',
     img: weather,
@@ -35,10 +36,28 @@ const dataProjects = [
     id: 3,
     technologies: 'JavaScript - HTML - Materialize-CSS',
     titulo: 'TO-DO LIST',
-    description: 'A basic To-Do List app developed with JavaScript',
+    description: 'A basic To-Do List app developed with JavaScript.',
     demoLink: 'https://gonjavi.github.io/JavascriptTodoList/',
     githubLink: 'https://github.com/gonjavi/JavascriptTodoList',
     img: todolist,
+  },
+  {
+    id: 4,
+    technologies: 'JavaScript - HTML - Materialize-CSS',
+    titulo: 'TIC-TAC-TOE',
+    description: 'A basic website with the Tic Tac Toe game based on javascript.',
+    demoLink: 'https://rawcdn.githack.com/gonjavi/JavascriptTicTacToe/d5f9510bcf3fc82556eb058b68bed6151782bb65/index.html',
+    githubLink: 'https://github.com/gonjavi/JavascriptTicTacToe',
+    img: tictactoe2,
+  },
+  {
+    id: 5,
+    technologies: 'JavaScript - HTML - Materialize-CSS',
+    titulo: 'TESTING TIC-TAC-TOE ',
+    description: 'This project is focus on unit testing with JavaScript.',
+    demoLink: 'https://rawcdn.githack.com/gonjavi/JavascriptTicTacToe/d5f9510bcf3fc82556eb058b68bed6151782bb65/index.html',
+    githubLink: 'https://github.com/gonjavi/JavaScriptTestingTictactoe',
+    img: tictactoe2,
   },
 ];
 
