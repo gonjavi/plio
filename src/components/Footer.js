625c112afe88d0e18b9cf47319759ef273b44532
manci <javiercaliescali@gmail.com>
2021-04-08 21:20:27 -0500

    actualizar footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,20 +5,22 @@ const Footer = () => (
   <div className="container foot">
     <div className="row">
       <ul className="list-inline letrasup col-lg-6 col-md-9 col-sm-12">
-        <li className="list-inline-item">Hello at gjavier.co</li>
+        <li className="list-inline-item">Hello at gjavier.com</li>
         <li className="list-inline-item menu">
           {' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://github.com/gonjavi">Github</a>
+          <a target="_blank" rel="noopener noreferrer" className="lista text-center" href="https://github.com/gonjavi">Github</a>
         </li>
         <li className="list-inline-item menu">
           {' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@javiercaliescali">Medium</a>
+          <a target="_blank" rel="noopener noreferrer" className="lista" href="https://medium.com/@javiercaliescali">Medium</a>
         </li>
         <li className="list-inline-item menu">
           {' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/g-mancillla">LinkedIn</a>
+          <a target="_blank" rel="noopener noreferrer" className="lista" href="https://www.linkedin.com/in/g-mancillla">LinkedIn</a>
         </li>
-        <Link to="/contact" className="list-inline-item">Contact</Link>
+        <li className="list-inline-item menu">
+          <Link to="/contact" className="list-inline-item lista">Contact</Link>
+        </li>
       </ul>
     </div>
     <div className="letras">Crafted using ReactJS, Bootstrap, and Visual Studio Code.</div>
